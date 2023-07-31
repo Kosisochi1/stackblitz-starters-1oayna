afc0300ff627a526fcbb9a9500e7fc8673ffdfca
Kosisochi1 <emmanuelezeoyirikosi@yahoo.com>
2023-07-31 23:59:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-1oayna
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-1oayna)
---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+console.log(__dirname);
+//This created the folder!!!
+// const filepath = path.join(__dirname, 'Student');
+// fs.mkdir(filepath, (err, res) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log('you are there ');
+// });
+// This created the file !!!
+// const filepath = path.join(__dirname, 'Student', 'user.js');
+// fs.writeFile(filepath, '', (err, res) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log('you are good');
+// });
